f2a0b85340b3e4accf502b23b6a276d57fe4bb58
Jody <jody@kaplon.us>
2024-01-25 09:35:17 -0500

    Change build-docker.sh to crate a dev image

--- bin/build-docker.sh
+++ bin/build-docker.sh
@@ -5,7 +5,7 @@ SERIES=${VERSION:0:4}-latest
 
 cat package.json | grep -v electron > server-package.json
 
-sudo docker build -t jkaplon/trilium:$VERSION --network host .
+sudo docker build -t jkaplon/trilium-dev:$VERSION --network host .
 
 #if [[ $VERSION != *"beta"* ]]; then
   #sudo docker tag zadam/trilium:$VERSION zadam/trilium:latest
